84b06e8bd717b5934dbc39d1f69d4b2cb5882a59
Ruan Jansen <ruan.d.jansen@gmail.com>
2024-06-23 20:38:43 +0200

    Initial Commit

---
+++ ProjectRubberDucky/ProjectRubberDuckyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectRubberDuckyApp.swift
+//  ProjectRubberDucky
+//
+//  Created by Ruan Jansen on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectRubberDuckyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
