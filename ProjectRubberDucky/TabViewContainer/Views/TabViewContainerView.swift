5b1215d0b97089cc93e65c74f708a3e1397a2c56
Ruan Jansen <ruan.d.jansen@gmail.com>
2024-08-05 19:21:16 +0200

    Fixed Tab selection bug

--- ProjectRubberDucky/TabViewContainer/Views/TabViewContainerView.swift
+++ ProjectRubberDucky/TabViewContainer/Views/TabViewContainerView.swift
@@ -3,10 +3,11 @@ import SwiftUI
 struct TabViewContainerView<Provider: FeatureProvider>: FeatureView where Provider.DataModel == [any Tabable] {
     @State var provider: Provider
     @State private var showErrorAlert: Bool
-
+    @State private var tabSelection: Int
     init(provider: Provider) {
         self.provider = provider
         self.showErrorAlert = false
+        self.tabSelection = 0
     }
 
     var body: some View {
@@ -15,12 +16,13 @@ struct TabViewContainerView<Provider: FeatureProvider>: FeatureView where Provid
             case .loading:
                 ProgressView()
             case .presentContent(using: let tabs):
-                TabView {
-                    ForEach(tabs, id: \.id) { tab in
+                TabView(selection: $tabSelection) {
+                    ForEach(Array(tabs.enumerated()), id: \.offset) { index, tab in
                         AnyView(tab.feature.featureView)
                             .tabItem {
                                 Label(tab.name, systemImage: tab.systemImage)
                             }
+                            .tag(index)
                     }
                 }
             case .error:
